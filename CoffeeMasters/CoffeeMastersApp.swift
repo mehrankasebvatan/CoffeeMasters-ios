6bcf3f2ed785f2c1857b23671bb2accdc3cbb7d9
Mehran Kasebvatan <mehran.kasebvatan@gmail.com>
2023-12-31 20:03:00 +0330

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Mehran Kasebvatan on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMasters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeMasters
+//
+//  Created by Mehran Kasebvatan on 12/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
